eef7c3671ccb335792d5806c66f05425669cc09f
pedrohfornari <pedrohkf14@gmail.com>
2017-04-19 19:28:20 -0300

    added wavelet filter banks to get the qrs complex and alpha, betha, teta and delta waves. added ECG segmentation by period using the R peak as reference

--- 02.Image_processing/Image_processing.m
+++ 02.Image_processing/Image_processing.m
@@ -3,31 +3,33 @@
  % Pedro Henrique Fornari
  % 13104320
 
+ clear all;
+ close all;
 %% Load Image
-I = imread('bigben.png');
-Igray = rgb2gray(I);
+I = imread('bigben.png'); %load image
+Igray = rgb2gray(I); %convert it to gray scale
 figure ('Name', 'Original Image');
 imshow(I);
 
 %% Apply bit plane slicing
-B = cell(8);
+B = cell(8,1);
 
 figure('Name', 'Bit Panel Slice')
 for i = 1:8
-    B{i}=zeros(size(Igray));
-    B{i}=bitset(B{i},i,bitget(Igray,i));
-    B{i}=uint8(B{i});
-    subplot(2, 4, i);
-    imshow(B{i}*255);
-    str = sprintf('Panel Bit %d', i);
+    B{i}=zeros(size(Igray)); %Pre set each cell
+    B{i}=bitset(B{i},i,bitget(Igray,i)); %get the selected bit
+    B{i}=uint8(B{i}); %transform it to an 8 bit integer
+    subplot(2, 4, i); 
+    imshow(B{i}*255); %multiply by 255 to correct the preset
+    str = sprintf('Panel Bit %d', i); %update title
     title(str);
 end
 
 %% Get RGB channels
 
-Rchannel = I(:, :, 1);
-Gchannel = I(:, :, 2);
-Bchannel = I(:, :, 3);
+Rchannel = I(:, :, 1); %Red channel is in the first dimension
+Gchannel = I(:, :, 2); %Green channel is in the second dimension
+Bchannel = I(:, :, 3); %Blue channel is in the third dimension
 
 figure
 subplot(1, 3, 1);
@@ -42,7 +44,10 @@ title('Blue Channel');
 
 %% Get gray image by manual conversion
 
+% define each value
 alfa = 0.2989; beta = 0.5870; gamma = 0.1140;
+
+% transform each pixel to gray scale
 for i = 1:size(I, 1)
     for j = 1:size(I, 2)
         Imgray(i, j) = alfa*I(i, j, 1) + beta*I(i, j, 2) + gamma*I(i, j, 3);
@@ -52,3 +57,37 @@ end
 figure('Name', 'Manual conversion from rgb to gray scale')
 imshow(Imgray);
 
+%% Get hue, saturation and value from a RGB image
+
+HSVImage = rgb2hsv(I);
+
+Hue = HSVImage(:,:,1);
+Saturation = HSVImage(:,:,2);
+Value = HSVImage(:,:,3);
+
+
+figure
+subplot(1, 3, 1);
+imshow(Hue);
+title('Hue');
+subplot(1, 3, 2);
+imshow(Saturation);
+title('Saturation');
+subplot(1, 3, 3);
+imshow(Value);
+title('Value');
+
+%% Do some adition operations with images
+
+%sum 100 to an image
+Iplus100 = imadd(I, 100);
+
+%subtract an image from another one
+A = imread('bigben.png');
+B = imread('edin_castle.png');
+AminusB = imsubtract(A,B);
+
+%invert image
+inverted = imadd(-A, 255);    
+
+%tresholding image
