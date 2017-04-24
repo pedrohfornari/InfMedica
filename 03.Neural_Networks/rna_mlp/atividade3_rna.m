d1398e51a554a30b3f334c54bb8931ebd0abf178
pedrohfornari <pedrohkf14@gmail.com>
2017-04-24 14:57:31 -0300

    implemented functions that train the networks

--- 03.Neural_Networks/rna_mlp/atividade3_rna.m
+++ 03.Neural_Networks/rna_mlp/atividade3_rna.m
@@ -14,9 +14,10 @@ local_rna = '~/Documents/InfMedica/03.Neural_Networks/rna_mlp/'; % pasta onde es
 % Padroes de entrada e saida para o treinamento da rede
 % load([local_rna,'pesos_pdrs.mat']);
 arq_dados_rna = [local_rna,'Padroes e pesos.xls'];  % endereco da planilha do Excel
-x = xlsread(arq_dados_rna,'padroes','B22:F119');    % padroes de entrada
-dk = xlsread(arq_dados_rna,'padroes','R22:V26');    % padroes de saida
-
+xfull = xlsread(arq_dados_rna,'padroes');    % padroes de entrada
+x = xfull(22:119 , 2:6);
+dkfull = xlsread(arq_dados_rna,'padroes','R22:V26');    % padroes de saida
+dk = dkfull(22:26 , 18:22);
 % Pesos sinopticos iniciais para a camada oculta e de saida
 w = xlsread(arq_dados_rna,'pesos'); % pesos iniciais
 wji = w(5:103 , 2:15); % pesos iniciais (camada oculta)
@@ -24,19 +25,19 @@ wkj = w(5:19 , 18:22); % pesos iniciais (camada de saida)
 qtde_padrao = size(x,2); % quantidade de padroes de treinamento
 
 % Parametros de configuracao da rede e do treinamento
-n1 = ;                                          % neuronios na camada de entrada
-n2 = ;                                          % neuronios na camada oculta
-n3 = ;                                          % neuronios na camada de saida
-tx_lrn = ;                                      % taxa de aprendizagem
-coef_m = ;                                      % coeficiente de momento
-erro_min_epc = ;                                % erro minimo por epoca
-max_epocas = ;                                  % maximo numero de epocas
-bias_1 = ;                                      % bias da camada oculta
-bias_2 = ;                                      % bias da camada de saida
+n1 = 98;                                         % neuronios na camada de entrada
+n2 = 14;                                         % neuronios na camada oculta
+n3 = 5;                                          % neuronios na camada de saida
+tx_lrn = 0.9;                                    % taxa de aprendizagem
+coef_m = 0;                                      % coeficiente de momento
+erro_min_epc = 0.001;                            % erro minimo por epoca
+max_epocas = 1000;                               % maximo numero de epocas
+bias_1 = 1;                                      % bias da camada oculta
+bias_2 = 1;                                      % bias da camada de saida
 
 % Inclinacao das funcoes de ativacao
-k1 = ;                                          % inclinacao da funcao na camada oculta
-k2 = ;                                          % inclinacao da funcao na camada de saida
+k1 = 1;                                          % inclinacao da funcao na camada oculta
+k2 = 1;                                          % inclinacao da funcao na camada de saida
 
 % Inicializacao de variaveis que armazenarao os erros da rede e dos padroes
 erro_pdr = zeros(max_epocas,qtde_padrao);
@@ -67,19 +68,21 @@ for epc = 1:max_epocas                          % p/ cada epoca de treinamento d
         
         % Ativacao dos neuronios da camada intermediaria (oculta)
         vj_aux = zeros(size(xi,1),n2);
-        for j = 1:n2;
+        for j = 1:n2
             vj_aux(:,j) = wji(:,j).*xi(:,pdr);
         end
         vj = sum(vj_aux)';                      % ativacao dos neuronios ocultos
         
         % Saida (yj) dos neuronios da camada intermediaria (oculta) -> equacao 1
-        
-        
+        yi = 1/(1+exp(-1*vj));
+    
         % Ativacao (vk) dos neuronios da camada de saida -> equacao 2
-        
-        
+        for k = 1:n3
+            vk_aux(:,k) = wkj(:,k).*yi(:, k);
+        end 
+        vk = sum(vk_aux)';
         % Saida (yk) dos neuronios da camada de saida -> equacao 3
-        
+        yk = 1/(1+exp(-1*vk));
         
         % Calculo dos erros local (ek) e do padrao (erro_pdr)
         % erro local -> erro de cada neuronio de saida
@@ -88,14 +91,14 @@ for epc = 1:max_epocas                          % p/ cada epoca de treinamento d
         erro_pdr(epc,pdr) = sum((ek.^2)/2);                                     
                 
         % Calculo dos erros da camada de saida (delta_k) -> equacao 4
-        
+        delta_k = (yk.*(1-yk)) .* ek;
         
         % calculo dos erros da camada oculta (delta_j), considerando que a funcao de
         % ativacao da a funcao logistica
         delta_j = (yj.*(1-yj)) .* (wkj(2:end,:) * delta_k);
         
         % Acumulo do ajuste dos pesos sinopticos (dwkj e dwji)
-        if pdr == 1;
+        if pdr == 1
            dwkj = tx_lrn .* (yj_aux * delta_k');
            dwji = tx_lrn .* (xi(:,pdr) * delta_j');
         else
@@ -104,34 +107,35 @@ for epc = 1:max_epocas                          % p/ cada epoca de treinamento d
         end
     end
     
-    % C�lculo do erro da �poca (erro_epc)
-    erro_epc(epc,1) = sum(erro_pdr(epc,:));      % erro da �poca
-    emq_epc(epc,1) = mean(erro_pdr(epc,:));      % erro m�dio da �poca
+    % Calculo do erro da epoca (erro_epc)
+    erro_epc(epc,1) = sum(erro_pdr(epc,:));      % erro da epoca
+    emq_epc(epc,1) = mean(erro_pdr(epc,:));      % erro medio da epoca
     
-    % Vizualiza��o do gr�fico com o erro de treinamento por �poca e por padr�o
+    % Vizualizacao do grafico com o erro de treinamento por epoca e por padrao
     set(findobj('tag','dados_rna'),'XData', (1:1:epc),'YData',erro_epc(1:epc,1));
     drawnow; pause(.05)
     if cancelar == 1
         break;
     end
-    % Verifica��o de crit�rio de parada: EMQ m�nimo por �poca
-    % se o EMQ da �poca � menor que erro_min_epc -> FIM DO TREINAMENTO
-    if ( emq_epc(epc,1) < erro_min_epc );
+    % Verificacao de criterio de parada: EMQ minimo por epoca
+    % se o EMQ da epoca eh menor que erro_min_epc -> FIM DO TREINAMENTO
+    if ( emq_epc(epc,1) < erro_min_epc )
         break;
     end
     
-    % C�lculo dos novos pesos sin�pticos (wkj_novo e wji_novo) -> equa��o 5
+    % Calculo dos novos pesos sinopticos (wkj_novo e wji_novo) -> equacao 5
     if epc == 1
-        
+        wkj_novo = wkj + dwkj + alpha * (wkj - wkj_antigo);
+        wji_novo = wji + dwji + alpha * (wji - wji_antigo);
     else
         
     end
     
-    % Atualiza��o dos pesos sin�pticos
+    % Atualizacao dos pesos sinopticos
     wkj_antigo = wkj;                           % pesos da camada oculta (t-1)
-    wji_antigo = wji;                           % pesos da camada de sa�da (t-1)
+    wji_antigo = wji;                           % pesos da camada de saida (t-1)
     wkj = wkj_novo;                             % pesos da camada oculta (t)
-    wji = wji_novo;                             % pesos da camada de sa�da (t)
+    wji = wji_novo;                             % pesos da camada de saida (t)
 end
 
 vetor_epc = get(findobj('tag','dados_rna'),'XData');
@@ -139,7 +143,7 @@ vetor_erro = get(findobj('tag','dados_rna'),'YData');
 close(grafico_rna);
 
 figure('NumberTitle','off','Color',[.94 .94 .94],...
-    'Name','Treinamento da RNA - gr�fico do erro da rede',...
+    'Name','Treinamento da RNA - grafico do erro da rede',...
     'Units','normalized', 'Position',[0.2556 0.2591 0.4392 0.5208]);
 plot(vetor_epc, vetor_erro, '-b', 'LineWidth',1.5)
 set(gca, 'FontSize', 8, 'Units','normalized', 'Position',[0.0715 0.11 0.9 0.8625]);
@@ -147,11 +151,11 @@ text(max(get(gca,'XLim')/2),max(get(gca,'YLim')/2),...
     ['Erro de treinamento = ',num2str(vetor_erro(end))],...
     'HorizontalAlignment','center','FontSize',9,'FontName','Verdana',...
     'Color',[0.847 0.161 0])
-xlabel('�pocas de treinamento','FontSize',9);
+xlabel('epocas de treinamento','FontSize',9);
 ylabel('Valor do erro','FontSize',9);
 
 % Final do treinamento
-salvar = questdlg('Deseja salvar a rede?','RNA', 'Sim','N�o','Sim');
+salvar = questdlg('Deseja salvar a rede?','RNA', 'Sim','Nao','Sim');
 if strcmp(salvar,'Sim') == 1
     arq_rna = [local_rna,'Rede neural.xls'];    % planilha com o resultado do treinamento
     [stat1,msg1] = xlswrite(arq_rna,wji,'Teste da rede','T5:AG103');
@@ -160,7 +164,7 @@ if strcmp(salvar,'Sim') == 1
         erro_pdr(1:epc,:)], 'Aprendizagem da rede','A2');
     if ( stat1 == 0 ) || ( stat2 == 0 ) || ( stat3 == 0 )
         disp(' '); disp('Exportar pesos ocultos:'); disp(msg1);
-        disp(' '); disp('Exportar pesos de sa�da:'); disp(msg2);
+        disp(' '); disp('Exportar pesos de saida:'); disp(msg2);
         disp(' '); disp('Exportar erros epc e pdr:'); disp(msg3); disp(' ');
         errordlg('Verifique o janela de comando','Erro ao exportar! Tente novamente...');
     else
